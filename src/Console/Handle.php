5363fb9d216061d1ad6bec7a2be930b612305ea5
AmrFekry <amrahmedfekry@outlook.com>
2020-03-21 00:59:37 +0100

    Add console

---
+++ src/Console/Handle.php
@@ -0,0 +1,16 @@
+<?php
+namespace bianky\Console;
+
+class Handle 
+{
+    /**
+     * Get requested command line
+     * 
+     * @param $argv
+     * @return void 
+     */
+    public function __construct($argv)
+    {
+
+    }
+}
